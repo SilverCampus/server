57c11684e1ff87b8d11055f95b22095fe81bce2c
JTH <nex_david@naver.com>
2023-08-10 16:07:58 +0900

    회원가입, 로그인 view 수정

--- api/views.py
+++ api/views.py
@@ -5,13 +5,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.tokens import RefreshToken
 
-# 회원가입
-@api_view(['GET', 'POST'])
+# 회원가입 (GET 필요 없어서 지우고 POST만 남겨 놓음)
+@api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def register(request): 
-    # if request.method == 'GET':
-    #     return Response({"message": "Please provide username, password, etc. to register."}, status=status.HTTP_200_OK)
-
     # Post
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
@@ -19,15 +16,18 @@ def register(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED) # 성공적 -> 201
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # valid하지 않으면 400 Error
 
-# 로그인
-@api_view(['GET', 'POST'])
+# 로그인 (GET 필요 없어서 지우고 POST만 남겨 놓음)
+@api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
-def login(request): # 로그인
-    if request.method == 'GET':
-        return Response({"message": "Please provide username and password to login."}, status=status.HTTP_200_OK)
+def login(request):
     
     username = request.data.get('username')
     password = request.data.get('password')
+
+    # 입력 검증: username과 password가 None인 경우 처리 (안정성 위해 추가)
+    if username is None or password is None:
+        return Response({"message": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
     user = authenticate(username=username, password=password)
     if user:
         refresh = RefreshToken.for_user(user)
@@ -35,4 +35,6 @@ def login(request): # 로그인
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         })
-    return Response(status=status.HTTP_401_UNAUTHORIZED)
+    # return Response(status=status.HTTP_401_UNAUTHORIZED)
+    return Response({"message": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+
