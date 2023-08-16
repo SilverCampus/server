b40b5a1edb755a8967a0eba032b4d3b7b6a38d66
kyubin0314@naver.com <kyubin0314@naver.com>
2023-08-16 20:42:26 +0900

    :sparkles: aws용 settings 적용

---
+++ config/settings/development.py
@@ -0,0 +1,23 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [] # 도메인, IP
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
